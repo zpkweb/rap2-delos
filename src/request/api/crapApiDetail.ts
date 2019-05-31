d92e9b2f1bf4940339c070df8ad460fdef1468a0
zpkweb <zpkafk@icloud.com>
2019-05-31 18:41:16 +0800

    modified crapapi

--- src/request/api/crapApiDetail.ts
+++ src/request/api/crapApiDetail.ts
@@ -1,8 +1,9 @@
-import fetch from 'node-fetch';
+import axios from 'axios';
 import { RequestUtils } from '../utils/request';
 import CrapApiService from '../../service/crapApi';
 export default async function (params: any) {
-  let target = `http://123.56.110.160:8181/front/interface/detail.do?&id=${params.id}&password=${params.password}&visitCode=${params.code}`;
-  let json = await fetch(target).then(res => res.json()).then(res => RequestUtils.complete(res))
+  let target = `http://123.56.110.160:8181/front/interface/detail.do?id=${params.id}&password=${params.password}&visitCode=${params.code}`;
+  target = `http://123.56.110.160:8181/front/interface/detail.do?&id=579ead79-4ff3-4b74-b70c-676db4618be4&password=www.zbgedu.com&visitCode=2989`;
+  let json = await axios.get( target ).then(res => res.data).then(res => RequestUtils.complete(res))
   return CrapApiService.detail(json);
 }
--- src/service/crapApi.ts
+++ src/service/crapApi.ts
@@ -4,17 +4,24 @@ export default class CrapApiService {
   public static menu(res: any) {
     res.data = _.find(res.data.menuList, (item: any) => {
       if (item.menu.id === "1b35d3a5-57fc-4fc7-b458-918523c33042") {
+        _.forEach(item.subMenu, (ele) => {
+          ele.modulesId = ele.menuUrl.split('/')[4];
+        })
         return item;
       }
     })
     return res;
   }
   public static modules(res: any) {
-    res.data = res.data.modules;
+    if (res.data) {
+      res.data = res.data.modules;
+    }
     return res;
   }
   public static interfaces(res: any) {
-    res.data = res.data.interfaces;
+    if (res.data) {
+      res.data = res.data.interfaces;
+    }
     return res;
   }
   public static detail(res: any) {
